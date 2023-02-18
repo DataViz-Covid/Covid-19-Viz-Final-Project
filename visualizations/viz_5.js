366a2790eef88c0320d184a89c8e672c67bfb131
claramouawad <clara1999m@gmail.com>
2023-02-18 23:23:08 +0100

    viz 5
    finished the map

--- visualizations/viz_5.js
+++ visualizations/viz_5.js
@@ -38,30 +38,16 @@ function drawChart_v5() {
     let g = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");     
 
+    let countries;
+
     // Load external data and boot
     d3.json("../data/countries.json").then( function(data) {
-
-        function getGroupedVal (func, ax) {
-            return func(data.features, function (d){
-                return func(d.geometry.coordinates, function (d1) {
-                    return d1[0][ax];
-                    
-                })
-            })
-        }
-
-        const max_x = getGroupedVal(d3.max, 0);
-        const max_y = getGroupedVal(d3.max, 1);
-        const min_x = getGroupedVal(d3.min, 0);
-        const min_y = getGroupedVal(d3.min, 1);
-
-        const ratio = (max_x-min_x) / (max_y-min_y);
-
+        
         // Map and projection
         const projection = d3.geoMercator().translate([width/2, height/1.8]);
         const path = d3.geoPath(projection);
 
-        let countries;
+        
 
         // Draw the map
         countries = g.append("g")
@@ -78,9 +64,91 @@ function drawChart_v5() {
             .style("stroke", "black")
             
 
+    
+
+
+        d3.csv("../data_clean/viz1.csv").then( function(dat) {
+
+            dat.forEach(function(d) {
+                d['total_deaths_per_million'] = +d['total_deaths_per_million'];
+            });
+
+            let data_dict = {}
+            dat.forEach (function(d) {
+                data_dict[d["location"]] = d["total_deaths_per_million"];
+            });
+
+            const max_count = d3.max(dat, d => d["total_deaths_per_million"]);
+            
+
+            const colors = d3.scaleThreshold()
+                .domain([10, 50, 100, 300, 500, 1000, 2000, 3000, 4000, max_count])
+                .range(d3.schemeGreens[9])
+
+
+            // Three function that change the tooltip when user hover / move / leave a cell
+            let mouseover = function (d) {
+                Tooltip.style("opacity", 1);
+            }
+            let mousemove = function (event, d) {
+                const loc = d.properties["ADMIN"];
+                const death_count = data_dict[loc];
+                Tooltip.html(loc + "<br>" + death_count)
+                .style("left", (event.pageX+20) + "px")
+                .style("top", (event.pageY) + "px");
+                console.log(loc, death_count)
+            }
+            let mouseleave = function (d) {
+                Tooltip.style("opacity", 0);
+            }
+
+            countries.attr("fill", function (d){ if (d.properties["ADMIN"] in data_dict) return colors(data_dict[d.properties["ADMIN"]]) ; else return "white" })
+                .on("mouseover", mouseover)
+                .on("mousemove", mousemove)
+                .on("mouseleave", mouseleave) 
+           
+            var legend = svg.selectAll('g.legendEntry')
+                .data(colors.range())
+                .enter()
+                .append('g').attr('class', 'legendEntry');
+        
+            legend
+                .append('rect')
+                .attr("x", 100)
+                .attr("y", function(d, i) {
+                return 30+ i * 30;
+                })
+            .attr("width", 20)
+            .attr("height", 20)
+            .style("stroke", "black")
+            .style("stroke-width", 1)
+            .style("fill", function(d){return d;}); 
+               //the data objects are the fill colors
+        
+             legend
+            .append('text')
+            .attr("x", 130) //leave 5 pixel space after the <rect>
+            .attr("y", function(d, i) {
+               return 30+ i * 30;
+            })
+            .attr("dy", "0.8em") //place text one line *below* the x,y point
+            .text(function(d,i) {
+                var extent = colors.invertExtent(d);
+                //extent will be a two-element array, format it however you want:
+                var format = d3.format("0.2f");
+                if( i == 0 ) return "< " + format(+extent[1]);
+                if(i == 8) return ">" + format(+extent[0]);
+                return format(+extent[0]) + " - " + format(+extent[1]);
+            });
+            legend.append('text')
+            .attr("x", 100)
+            .attr("y", 20)
+            .text("Deaths per million") 
+
+
     })
 
 
-
+    })
 }
 drawChart_v5();
