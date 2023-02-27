28a0114798ed55ad50f7ebf5fcd372a86d676c16
claramouawad <clara1999m@gmail.com>
2023-02-27 19:47:53 +0100

    add viz2, viz3, viz8 and viz11 and some changes for home page

--- visualizations/viz_11.js
+++ visualizations/viz_11.js
@@ -1,6 +1,149 @@
-function drawChart_v11() {
-    const div_id = "v11";
-    // Deniz - Bar plot showing inflation delta from before to after covid
+// Define the margins of the chart
+var margin = { top: 10, right: 20, bottom: 50, left: 120 };
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+var container = d3.select("#chart").node().getBoundingClientRect();
 
-}
-drawChart_v11();
+// Create scales for the x and y axes
+var xScale = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+var yScale = d3.scaleLinear().rangeRound([height, 2]);
+
+// Create axes for the x and y scales
+var xAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.timeFormat("%b %Y"));
+var yAxis = d3.axisLeft(yScale);
+
+// Select the SVG element and set its dimensions and margins
+var svg = d3.select("#chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("viz11.json").then(function(data) {
+
+    // Define the date parser
+    var parseDate = d3.timeParse("%Y-%m");
+
+    // Convert the Date strings to Date objects
+    data.forEach(function(d) {
+        d.Date = parseDate(d.Date);
+    });
+
+    // Create axes for the x and y scales
+    var xAxis = d3.axisBottom(xScale)
+        .tickValues(data.filter(function(d, i) {
+            var month = d.Date.getMonth();
+            return month % 2 === 0;
+        }).map(function(d) { return d.Date; }))
+        .tickFormat(d3.timeFormat("%b %Y"));
+
+    // Extract the list of countries from the JSON data
+    var countries = [...new Set(data.map(d => d.Country))];
+
+    // Create a dropdown menu for selecting the country
+    var dropdown = d3.select("#dropdown")
+        .append("select")
+        .on("change", updateChart);
+
+    dropdown.selectAll("option")
+        .data(countries)
+        .enter()
+        .append("option")
+        .text(function(d) { return d; });
+
+    // Set the initial country to display
+    var initialCountry = countries[0];
+
+    function updateChart() {
+        // Get the selected country from the dropdown menu
+        var selectedCountry = dropdown.property("value");
+
+        // Filter the data to include only the data for the selected country
+        var filteredData = data.filter(function(d) {
+            return d.Country === selectedCountry;
+        });
+
+        // Set the domain of the x and y scales
+        xScale.domain(filteredData.map(function(d) {
+            return d.Date;
+        }));
+
+        // Use d3.extent() to get the min and max inflation rates from filteredData
+        var [minRate, maxRate] = d3.extent(filteredData, function(d) {
+            return d.InflationRate;
+        });
+
+        // Set the domain of the y scale to include negative values if minRate is negative
+        yScale.domain(minRate < 0 ? [minRate, maxRate] : [0, maxRate]);
+
+
+            // Remove the old chart elements
+        svg.selectAll("*").remove();
+
+        // Append a group element for the chart
+        var chartGroup = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // Append the x and y axes to the chart
+        chartGroup.append("g")
+            .attr("class", "x-axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("transform", "rotate(-45)") // change the rotation angle here
+            .attr("text-anchor", "end")
+            .attr("dx", "-0.8em")
+            .attr("dy", "-0.15em");
+
+// Add a label for the x axis
+        chartGroup.append("text")
+            .attr("class", "x-axis-label")
+            .attr("x", width / 2)
+            .attr("y", height + margin.bottom +10)
+            .style("text-anchor", "middle")
+            .text("Date");
+
+// Add a label for the y axis
+        chartGroup.append("text")
+            .attr("class", "y-axis-label")
+            .attr("x", -height / 2)
+            .attr("y", -margin.left+70)
+            .attr("transform", "rotate(-90)")
+            .style("text-anchor", "middle")
+            .text("Inflation Rate");
+
+        chartGroup.append("g")
+            .attr("class", "y-axis")
+            .call(yAxis);
+
+        // Define a color scale based on the inflation rate
+        var colorScale = d3.scaleSequential()
+            .interpolator(d3.interpolatePlasma);
+
+
+
+        // Add rectangles to the SVG
+        chartGroup.selectAll(".bar")
+            .data(filteredData)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) {
+                return xScale(d.Date);
+            })
+            .attr("y", function(d) {
+                return yScale(Math.max(0, d.InflationRate));
+            })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d) {
+                return Math.abs(yScale(d.InflationRate) - yScale(0));
+            })
+            .style("fill", function(d) {
+                return colorScale(d.InflationRate);
+            })
+
+    }
+    updateChart(initialCountry);
+
+});
--- visualizations/viz_8.js
+++ visualizations/viz_8.js
@@ -1,6 +1,141 @@
-function drawChart_v8() {
-    const div_id = "v8";
-    // Deniz - Small multiple Pie chart of vax brand distrib per country
+const margin = { top: 30, right: 100, bottom: 50, left: 300 };
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+// Load data from JSON file
+d3.json("viz8.json").then(function(data) {
+    // Extract countries from data
+    var countries = data.data.map(function(d) { return d.Country; });
 
-}
-drawChart_v8();
+    // Populate dropdown menu
+    var select = d3.select("#country-select");
+    select.selectAll("option")
+        .style("position", "absolute")
+        .style("top", margin.top + "px")
+        .style("left", margin.left + "px")
+        .data(countries)
+        .enter()
+        .append("option")
+        .text(function(d) { return d; });
+
+    function drawPieChart(country) {
+        // Extract data for selected country
+        var selectedData = data.data.find(function(d) { return d.Country === country; });
+        var vaccines = JSON.parse(selectedData.vaccines.replace(/'/g, '"'));
+
+        // Compute total number of doses
+        var totalDoses = parseInt(selectedData.totalDoses);
+
+
+        // Compute data for pie chart
+        var pieData = vaccines.map(function(d) {
+            return {
+                label: d.name,
+                value: d.count
+            };
+        });
+
+        // Compute percentages for each vaccine type
+        var totalVaccineDoses = d3.sum(pieData, function(d) { return d.value; });
+        pieData.forEach(function(d) {
+            d.percent = Math.round(d.value / totalVaccineDoses * 100);
+            d.value = d.value / totalDoses;
+        });
+
+        // Set dimensions for pie chart
+        var width = 300;
+        var height = 300;
+        var radius = Math.min(width, height) / 2;
+
+        // Set color scale for pie chart
+        var color = d3.scaleOrdinal()
+            .domain(pieData.map(function(d) { return d.label; }))
+            .range( d3.schemePaired );
+
+
+
+        // Define arc for pie chart
+        var arc = d3.arc()
+            .outerRadius(radius - 10)
+            .innerRadius(0);
+
+        // Define pie layout
+        var pie = d3.pie()
+            .sort(null)
+            .value(function(d) { return d.value; });
+
+        // Clear previous chart
+        d3.select("#pie-chart").selectAll("*").remove();
+
+        // Append main SVG element
+        var svg = d3.select("#pie-chart")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
+            .append("g")
+            .attr("transform", "translate(" + (margin.left + width / 2) + "," + (margin.top + height / 2) + ")");
+
+        // Append pie slices
+        var arcs = svg.selectAll(".arc")
+            .data(pie(pieData))
+            .enter()
+            .append("g")
+            .attr("class", "arc");
+
+        var hoverColor = d3.scaleOrdinal()
+            .domain(pieData.map(function(d) { return d.label; }))
+            .range(pieData.map(function(d) { return d3.rgb(color(d.label)).darker(); }));
+
+
+        arcs.append("path")
+            .attr("d", arc)
+            .attr("fill", function(d) { return color(d.data); })
+            .on("mouseover", function(d) {
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .attr("fill", "darkgreen");
+            })
+            .on("mouseout", function(d) {
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .attr("fill", function(d) { return color(d.data); });
+            })
+
+
+
+            .append("title")
+            .text(function(d) {
+                var doses = d.data.label + ": " + Math.round(d.data.value * totalDoses) + " doses";
+                return doses + " (" + d.data.percent + "%)";
+            })
+            .attr("transform", function(d) {
+                var pos = arc.centroid(d);
+                pos[1] += 20; // adjust vertical position of tooltip
+                return "translate(" + pos + ")";
+            })
+            .attr("dy", "0.35em");
+
+
+        function midAngle(d) {
+            return d.startAngle + (d.endAngle - d.startAngle) / 2;
+        }
+    }
+
+
+
+
+
+    // Initialize pie chart with first country
+    drawPieChart(countries[0]);
+
+// Redraw pie chart when dropdown selection changes
+    select.on("change", function() {
+        // Clear previous chart
+        d3.select("#pie-chart").selectAll("*").remove();
+
+        var country = d3.select(this).property("value");
+        drawPieChart(country);
+    });
+
+});
